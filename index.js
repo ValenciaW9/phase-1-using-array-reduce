f424e394bb44f3999fdd7e5a167c73198691a0d3
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-12 21:50:42 -0400

    phase-1-using-array-reduce/
    phase-1-using-array-reduce/

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const batteryBatches = [4, 5, 6, 3, 2, 7];
+const batteryBatches = [4, 5, 6, 3, 2, 7, 4];
 
 const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => {
   return accumulator + currentValue;
 }, 0);
 
-console.log(totalBatteries); // Output: 27
+console.log(totalBatteries); // Output: 31
